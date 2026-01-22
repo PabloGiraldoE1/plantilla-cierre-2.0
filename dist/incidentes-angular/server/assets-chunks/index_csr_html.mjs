8b8cea0fcd10bddf251595636380c91160c106b4
PabloGiraldoE1 <jpgiraldo@personalsoft.com>
2026-01-21 20:48:13 -0500

    borrar-cloud

---
+++ dist/incidentes-angular/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>IncidentesAngular</title>
+  <base href="/plantilla-cierre-2.0/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<style>*{margin:0;padding:0;box-sizing:border-box}body{font-family:Segoe UI,Tahoma,Geneva,Verdana,sans-serif;margin:0;padding:0;background:#f5f7fa;color:#333;line-height:1.6}</style><link rel="stylesheet" href="styles-NRLRE5AK.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-NRLRE5AK.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-2ZPLEWSK.js" type="module"></script></body>
+</html>
+`;
